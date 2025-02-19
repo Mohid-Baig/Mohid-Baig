04334b817080b330082ea2f94c4732ec54146706
mohid <mohidtahir12@gmail.com>
2025-02-19 15:37:14 +0500

    add

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { EXPERIENCES } from "../constants";
+
 const Experience = () => {
   return (
     <div className="pb-4">
@@ -13,12 +14,12 @@ const Experience = () => {
       </motion.h2>
       <div>
         {EXPERIENCES.map((experience, index) => (
-          <div className="mb-8 flex flex-wrap lg:justify-center">
+          <div className="mb-8 flex flex-col lg:flex-row lg:justify-start items-center lg:items-start">
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+              className="w-full lg:w-1/4 text-center lg:text-left"
             >
               <p className="mb-2 text-sm text-stone-400">{experience.year}</p>
             </motion.div>
@@ -26,7 +27,7 @@ const Experience = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
+              className="w-full max-w-xl lg:w-3/4 text-center lg:text-left"
             >
               <h3 className="mb-2 font-semibold">
                 {experience.role} -{" "}
@@ -35,7 +36,7 @@ const Experience = () => {
                 </span>
               </h3>
               <p className="mb-4 text-stone-400">{experience.description}</p>
-              <div className="flex justify-start items-center flex-wrap gap-2">
+              <div className="flex justify-center lg:justify-start items-center flex-wrap gap-2">
                 {experience.technologies.map((tech, index) => (
                   <span
                     className="mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300"
